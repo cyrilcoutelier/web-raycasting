8a0eaf2fd7bb2019a3247011da9566461e032ce8
Cyril Coutelier <cyril@flat.io>
2019-09-19 17:41:26 +0200

    chore(initial): initial setup of the project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /test\.js$/,
+        use: 'mocha-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
+};
